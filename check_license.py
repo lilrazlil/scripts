c1b7cde3550f1f169fd2635397e6f4a0e9271dce
lilrazlil <nikmoshkin.2002@gmail.com>
2022-09-19 12:50:04 +0700

    add ru language kesl

--- check_license.py
+++ check_license.py
@@ -6,18 +6,22 @@ from time import gmtime, strftime
 data_now = strftime("%Y-%m-%d", gmtime())
 
 def data_license_kesl():
-    word="License expiration date:"
-    f = subprocess.check_output(['kesl-control','--app-info']).decode("utf-8").split(sep="\n")
+    word_kesl_en="License expiration date:"
+    word_kesl_ru="Дата окончания срока действия лицензии:"
+    f = subprocess.check_output(['cat','test_ru.txt']).decode("utf-8").split(sep="\n")
     for line in f:
-        if str(line).find(word) != -1:
-            return line
+        if str(line).find(word_kesl_en ) != -1:
+            return re.findall('\d+', line)
+
+        elif str(line).find(word_kesl_ru ) != -1:
+            return re.findall('\d+', line)
 
 def data_license_sn():
-    word="UpgrExp="
-    f = subprocess.check_output(['cat','/opt/secretnet/etc/serial']).decode("utf-8").split(sep="\n")
+    word_sn="UpgrExp="
+    f = subprocess.check_output(['cat','test_sn.txt']).decode("utf-8").split(sep="\n")
     for line in f:
-        if str(line).find(word) != -1:
-            return line
+        if str(line).find(word_sn) != -1:
+            return re.findall('\d+', line)
 
 def days_between(d1, d2):
     d1 = datetime.strptime(d1, "%Y-%m-%d")
@@ -25,14 +29,12 @@ def days_between(d1, d2):
     return (d2 - d1)
 
 def diff_days_kesl():
-    data_kesl=re.findall('\d+', str(data_license_kesl()))
-    data_kesl= '-'.join(data_kesl)
+    data_kesl= '-'.join(data_license_kesl())
     day_kesl = str(days_between(data_now,data_kesl)).split(sep=' ')
     return day_kesl[0]
 
 def diff_days_sn():
-    data_sn=re.findall('\d+', str(data_license_sn()))
-    data_sn= '-'.join(data_sn)
+    data_sn= '-'.join(data_license_sn())
     day_sn = str(days_between(data_now,data_sn)).split(sep=' ')
     return day_sn[0]
 
