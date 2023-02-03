7055c4ddfd08dd65d7e06e8933af0b05c7219500
lilrazlil <67465044+lilrazlil@users.noreply.github.com>
2023-02-03 12:04:03 +0700

    add var DIRECTORY

--- db_backups.sh
+++ db_backups.sh
@@ -3,6 +3,7 @@
 PIDFILE=/tmp/backup.pid
 TIMESTAMP=$(date '+%F-%H-%M')
 LIMIT=1
+DIRECTORY=/u00/basebackup
 if [ -f $PIDFILE ]; then
                 if pgrep -F $PIDFILE &>/dev/null; then
                         exit 1
@@ -12,20 +13,20 @@ if [ -f $PIDFILE ]; then
 fi
 
 get_count_backups(){
-COUNT=$(ls /u00/basebackup/ | grep "^20*" | wc -l)
+COUNT=$(ls $DIRECTORY | grep "^20*" | wc -l)
 }
 
 echo "$TIMESTAMP:start backup"
 
-rm -rf /u00/archive_wal/*
+#rm -rf /u00/archive_wal/*
 
-echo "$TIMESTAMP:delete wal"
+#echo "$TIMESTAMP:delete wal"
 
-mkdir -p /u00/basebackup/$TIMESTAMP > /dev/null 2>&1
+mkdir -p $DIRECTORY/$TIMESTAMP > /dev/null 2>&1
 
 echo "$TIMESTAMP:pg_basebackup start"
 
-pg_basebackup -Ft -z -D /u00/basebackup/$TIMESTAMP
+pg_basebackup -Ft -z -D $DIRECTORY/$TIMESTAMP
 
 echo "$TIMESTAMP:pg_basebackup end"
 
@@ -35,8 +36,8 @@ echo "$TIMESTAMP:check count backups"
 
 while [ "$COUNT" -gt "$LIMIT" ]
 	do
-		last_backup=$(ls /u00/basebackup/ | grep "^20*" | head -n1 )
-		rm -rf /u00/basebackup/$last_backup;
+		last_backup=$(ls $DIRECTORY | grep "^20*" | head -n1 )
+		rm -rf $DIRECTORY/$last_backup;
 		echo "remove $last_backup"
 		get_count_backups
 	done
